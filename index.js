8af5826c3ff96bb18414016dbf6032aa072db084
Piplup0393 <itsukiitakura393@gmail.com>
2024-12-27 18:34:15 +0900

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["/free boost", "/free boost"];
+const statusMessages = ["wp bot", "wp bot"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
